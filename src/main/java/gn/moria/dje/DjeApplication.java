faa94cdb315401494790ec0dd7b7bb5952201f97
kukufuckingkan <keitakukukhan@gmail.com>
2024-10-18 23:16:19 +0000

    djene

---
+++ src/main/java/gn/moria/dje/DjeApplication.java
@@ -0,0 +1,13 @@
+package gn.moria.dje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gn/moria/dje/DjeApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.moria.dje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
